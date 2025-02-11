29a26f5d2149380c1e7743ed81a08421c9e3c41e
Alperenissimo <62278757+Alperenissimo@users.noreply.github.com>
2025-02-11 12:12:24 +0300

    Add files via upload

---
+++ system/flcore/clients/client_alp_4th_algo_attention.py
@@ -0,0 +1,214 @@
+from collections import defaultdict
+import copy
+import torch
+import torch.nn as nn
+import numpy as np
+import time
+from flcore.clients.clientbase import Client
+
+
+class clientalp_v4(Client):
+    def __init__(self, args, id, train_samples, test_samples, **kwargs):
+        super().__init__(args, id, train_samples, test_samples, **kwargs)
+
+        self.protos = None
+        self.global_protos = None
+        self.loss_mse = nn.MSELoss()
+        self.weights = None
+
+        self.lamda = args.lamda
+
+
+    def train(self):
+        trainloader = self.load_train_data()
+        start_time = time.time()
+
+        # self.model.to(self.device)
+        self.model.train()
+
+        max_local_epochs = self.local_epochs
+        if self.train_slow:
+            max_local_epochs = np.random.randint(1, max_local_epochs // 2)
+
+        for param in self.model.base.parameters():
+            param.requires_grad = False
+        for param in self.model.head.parameters():
+            param.requires_grad = True
+
+        for i, (x, y) in enumerate(trainloader):
+            if type(x) == type([]):
+                x[0] = x[0].to(self.device)
+            else:
+                x = x.to(self.device)
+            y = y.to(self.device)
+            if self.train_slow:
+                time.sleep(0.1 * np.abs(np.random.rand()))
+            rep = self.model.base(x)
+            output = self.model.head(rep)
+            loss = self.loss(output, y)
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+        for param in self.model.base.parameters():
+            param.requires_grad = True
+        for param in self.model.head.parameters():
+            param.requires_grad = False
+            
+        # protos = defaultdict(list)
+        for step in range(max_local_epochs):
+            for i, (x, y) in enumerate(trainloader):
+                if type(x) == type([]):
+                    x[0] = x[0].to(self.device)
+                else:
+                    x = x.to(self.device)
+                y = y.to(self.device)
+                if self.train_slow:
+                    time.sleep(0.1 * np.abs(np.random.rand()))
+                rep = self.model.base(x)
+                output = self.model.head(rep)
+                loss = self.loss(output, y)
+
+                if self.global_protos is not None:
+                    proto_new = copy.deepcopy(rep.detach())
+                    for i, yy in enumerate(y):
+                        y_c = yy.item()
+                        if type(self.global_protos[y_c]) != type([]):
+                            proto_new[i, :] = self.global_protos[y_c].data
+                    loss += self.loss_mse(proto_new, rep) * self.lamda
+
+                # for i, yy in enumerate(y):
+                #     y_c = yy.item()
+                #     protos[y_c].append(rep[i, :].detach().data)
+
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+
+
+
+
+        self.collect_protos()
+        #self.protos,self.label_weights = agg_func(protos)
+
+        if self.learning_rate_decay:
+            self.learning_rate_scheduler.step()
+
+        self.train_time_cost['num_rounds'] += 1
+        self.train_time_cost['total_cost'] += time.time() - start_time
+
+
+    def collect_protos(self):
+        trainloader = self.load_train_data()
+        self.model.eval()
+
+        protos = defaultdict(list)
+        with torch.no_grad():
+            for i, (x, y) in enumerate(trainloader):
+                if type(x) == type([]):
+                    x[0] = x[0].to(self.device)
+                else:
+                    x = x.to(self.device)
+                y = y.to(self.device)
+                if self.train_slow:
+                    time.sleep(0.1 * np.abs(np.random.rand()))
+                rep = self.model.base(x)
+
+                for i, yy in enumerate(y):
+                    y_c = yy.item()
+                    protos[y_c].append(rep[i, :].detach().data)
+
+        self.protos,self.label_weights = agg_func(protos)
+
+    def set_protos(self, global_protos):
+        
+        self.global_protos = copy.deepcopy(global_protos)
+
+    def set_head(self, head):
+        for new_param, old_param in zip(head.parameters(), self.model.head.parameters()):
+            old_param.data = new_param.data.clone()
+
+
+    def test_metrics(self):
+        testloaderfull = self.load_test_data()
+        # self.model = self.load_model('model')
+        # self.model.to(self.device)
+        self.model.eval()
+
+        test_acc = 0
+        test_num = 0
+        
+        if self.global_protos is not None:
+            with torch.no_grad():
+                for x, y in testloaderfull:
+                    if type(x) == type([]):
+                        x[0] = x[0].to(self.device)
+                    else:
+                        x = x.to(self.device)
+                    y = y.to(self.device)
+                    rep = self.model.base(x)
+
+                    output = float('inf') * torch.ones(y.shape[0], self.num_classes).to(self.device)
+                    for i, r in enumerate(rep):
+                        for j, pro in self.global_protos.items():
+                            if type(pro) != type([]):
+                                output[i, j] = self.loss_mse(r, pro)
+
+                    test_acc += (torch.sum(torch.argmin(output, dim=1) == y)).item()
+                    test_num += y.shape[0]
+
+            return test_acc, test_num, 0
+        else:
+            return 0, 1e-5, 0
+
+    def train_metrics(self):
+        trainloader = self.load_train_data()
+        # self.model = self.load_model('model')
+        # self.model.to(self.device)
+        self.model.eval()
+
+        train_num = 0
+        losses = 0
+        with torch.no_grad():
+            for x, y in trainloader:
+                if type(x) == type([]):
+                    x[0] = x[0].to(self.device)
+                else:
+                    x = x.to(self.device)
+                y = y.to(self.device)
+                rep = self.model.base(x)
+                output = self.model.head(rep)
+                loss = self.loss(output, y)
+
+                if self.global_protos is not None:
+                    proto_new = copy.deepcopy(rep.detach())
+                    for i, yy in enumerate(y):
+                        y_c = yy.item()
+                        if type(self.global_protos[y_c]) != type([]):
+                            proto_new[i, :] = self.global_protos[y_c].data
+                    loss += self.loss_mse(proto_new, rep) * self.lamda
+                train_num += y.shape[0]
+                losses += loss.item() * y.shape[0]
+
+        return losses, train_num
+
+
+# https://github.com/yuetan031/fedproto/blob/main/lib/utils.py#L205
+def agg_func(protos):
+    """
+    Returns the average of the weights.
+    """
+    weights = {}
+    for [label, proto_list] in protos.items():
+        if len(proto_list) > 1:
+            proto = 0 * proto_list[0].data
+            for i in proto_list:
+                proto += i.data
+            protos[label] = proto / len(proto_list)
+            weights[label] = len(proto_list)
+        else:
+            protos[label] = proto_list[0]
+            weights[label] = 1
+    
+    return protos,weights
